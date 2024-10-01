ed4c243331134207c377ddfbf853997b0916acac
KoniDevs <delgadoargie04@gmail.com>
2024-10-01 17:04:37 +0800

    chore: add delete routes

--- server/routes/mealPlan.route.js
+++ server/routes/mealPlan.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getMealPlan,
   saveMealPlan,
+  removeMealPlan
 } = require("../controllers/mealPlanner.controller.js");
 
 const router = express.Router();
 
 router.get("/:userId", getMealPlan);
 router.post("/save-meal-plan", saveMealPlan);
+router.delete("/:userId", removeMealPlan);
 
 module.exports = router;
