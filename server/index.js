86ece84ff7d66074c8f51723404c330ca8aba7e8
KoniDevs <delgadoargie04@gmail.com>
2024-10-01 17:00:28 +0800

    chore: add workout and workout planner, modify version

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ const authRoutes = require("./routes/auth.route.js");
 const jobOffers = require("./routes/offers.route.js");
 const mealsRoutes = require("./routes/meal.route.js");
 const mealPlanRoutes = require("./routes/mealPlan.route.js");
+const workoutPlanRoutes = require("./routes/workoutPlan.route.js");
+const workoutRoutes = require("./routes/workout.route.js");
+
 const app = express();
 
 app.use(
@@ -28,18 +31,22 @@ app.use(
     frameguard: { action: "deny" },
     hsts: {
       maxAge: 31536000, // One year in seconds
-      includeSubDomains: true, // Apply to all subdomains
+      includeSubDomains: true,
       preload: true,
     },
     referrerPolicy: { policy: "no-referrer" },
   })
 );
+
 app.set("trust proxy", 1);
 app.disable("x-powered-by");
+
 app.use((req, res, next) => {
   res.removeHeader("Server");
   next();
 });
+
+// Enable CORS
 app.use(
   cors({
     origin: "*",
@@ -48,14 +55,19 @@ app.use(
     credentials: true,
   })
 );
+
+// Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 1000,
 });
 app.use(limiter);
+
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use('/workoutVideos', express.static(path.join(__dirname, 'public', 'workoutVideos')));
 
 app.use("/auth", authRoutes);
 app.use("/contact", contactRoutes);
@@ -63,6 +75,8 @@ app.use("/job", jobOffers);
 app.use("/client", usersRoutes);
 app.use("/meals", mealsRoutes);
 app.use("/meal-planner", mealPlanRoutes);
+app.use("/workouts", workoutRoutes);
+app.use("/workout-planner", workoutPlanRoutes);
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -72,7 +86,7 @@ app.get("/*", (req, res) => {
 
 connectDB()
   .then(() => {
-    app.listen(4000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is now live.");
     });
   })
